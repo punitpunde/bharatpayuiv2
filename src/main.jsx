a1c0b02599844a66711dd2388c379beb32783464
Punit Punde <punitpunde@gmail.com>
2024-10-01 23:11:54 +0530

    This is updated version with responsiveness

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import App from "./App.jsx";
