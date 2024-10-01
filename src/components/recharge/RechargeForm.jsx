8e6106f4618257ba4731b451ee8dcc0850329b0d
Punit Punde <punitpunde@gmail.com>
2024-10-01 23:25:00 +0530

    This is updated version with responsiveness

--- src/components/recharge/RechargeForm.jsx
+++ src/components/recharge/RechargeForm.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 function RechargeForm() {
   return (
-    <div className="recharge-form-container col-md-4 col-10 ps-4 pt-4 pb-5 d-flex flex-column text-center">
+    <div className="recharge-form-container col-md-4 col-12 ps-4 pt-4 pb-5 d-flex flex-column text-center">
       <h4 className="fs-5 pt-3">Recharge or Pay Mobile Bills</h4>
       <form action="">
         <div className="input-box mx-auto">
